e574686de998be7574a01d48a94525e56708bac8
Marcin Kocot <mkocot@k.pl>
2019-09-17 17:32:44 +0200

    double head support, slight border tuning

---
+++ config/i3/screens.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+xrandr --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal
+
+if (xrandr | grep -q "VGA1 connected"); then
+	xrandr --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+fi
